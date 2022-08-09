fbce73536e38868c06894edd0cbf9bc8834f1d44
JeffChristian <jeffchristiansantos@gmail.com>
2022-08-08 22:41:04 -0300

    adição do botão de curriculo na introdução principal da página

--- src/app/components/introduction/introduction.component.ts
+++ src/app/components/introduction/introduction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-introduction',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./introduction.component.css']
 })
 export class IntroductionComponent implements OnInit {
+
+  faDownload = faDownload;
 
   constructor() { }
 
