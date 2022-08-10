2fb033165472ae251a2d50ccedd5d8d8ac57c8d5
JeffChristian <jeffchristiansantos@gmail.com>
2022-08-10 00:12:40 -0300

    card de apresentação extra, adição no leia mais com informação de currículo

--- src/app/components/apresentation/apresentation.component.ts
+++ src/app/components/apresentation/apresentation.component.ts
@@ -15,4 +15,18 @@ export class ApresentationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  show(e: any){
+    let card = document.querySelector('#hidden');
+    card?.setAttribute('id', 'show');
+    let sobre = document.querySelector('.sobre') as HTMLBodyElement ;
+    sobre.style.fontSize = "13px";
+  }
+
+  hidden(e: any){
+    let card = document.querySelector('.nav-card');
+    card?.setAttribute('id', 'hidden');
+    let sobre = document.querySelector('.sobre') as HTMLBodyElement ;
+    sobre.style.fontSize = "1em";
+  }
+
 }
