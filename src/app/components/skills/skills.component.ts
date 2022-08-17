42133deb024374df1c9ceba9b486c98c2e2f7ee5
JeffChristian <jeffchristiansantos@gmail.com>
2022-08-16 23:30:39 -0300

    área de habilidades com respectivas barras de progressão e informação por atributo de área

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -13,6 +13,27 @@ export class SkillsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const skillsContent = document.getElementsByClassName('skills-content'), skillsHeader = document.querySelectorAll('.skills-header');
+
+    function toggleSkills(this: any){
+
+      let itemClass = this.parentNode.className
+
+      for(let i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills-content skills-close';
+      }
+
+      if(itemClass === 'skills-content skills-close'){
+        this.parentNode.className = 'skills-content skills-open';
+      }
+
+    }
+
+    skillsHeader.forEach((el) =>{
+      el.addEventListener('click', toggleSkills)
+    })
+
   }
 
 }
